9640d89642fd52dc8ce01cc4d7dca620616b360c
Rayan-Harbi <45786523+Rayan-Harbi@users.noreply.github.com>
2019-01-21 00:25:18 +0300

    Update d1.js

--- d1.js
+++ d1.js
@@ -354,7 +354,7 @@ client.on('message', message => {
         .setColor("WHITE")
         .setFooter(` `)
         message.channel.send({embed})
-        message.p.send({embed})
+        p.send(embed)
             message.delete();
         log.send({embed});
     }
