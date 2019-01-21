c08f789735be1f7222477bccd4cb98eae25196b0
Rayan-Harbi <45786523+Rayan-Harbi@users.noreply.github.com>
2019-01-21 15:56:24 +0300

    Update d1.js

--- d1.js
+++ d1.js
@@ -374,7 +374,7 @@ client.on('message', msg => {
 client.on('message', msg => {
      if(msg.content.startsWith(`unban${prefix}`)){
         let bUser = msg.mentions.users.first()
-        if(!bUser) return msg.replay(`unban! <mention>`).then( msgs => msgs.delete(3000));
+        if(!bUser) return msg.reply(`unban! <mention>`).then( msgs => msgs.delete(3000));
          if(!msg.member.hasPermission("BAN_MEMBERS")) return msg.reply("you don't have permission").then(s => {s.delete(1600);})
             if(!msg.guild.me.hasPermission("BAN_MEMBERS")) return msg.reply("i don't have permission").then(z => {z.delete(1600);})      
             let banembed = new Discord.RichEmbed()
